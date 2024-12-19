dd000dd668fcc47c4b243a71a74db4e090005571
Jose Navarro <jfnr398@gmail.com>
2024-12-19 11:41:40 -0500

    Backend v1.7

--- src/main/resources/static/src/controller/api.js
+++ src/main/resources/static/src/controller/api.js
@@ -1,28 +1,48 @@
 import { alertShow } from "../../assets/js/util.js";
 
 export function sendData(action, url, body) {
-    return fetch(`https://adequate-art-production-2380.up.railway.app/api/${url}`, {
-            method: action,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(body),
-        })
-        .catch(error => {
-            alertShow("Error", "Inconveniente al enviar los datos, se actualizará en breve", "error");
-            setTimeout(() =>{
-                window.location.reload();
-            }, 3000)
-        });
+    checkServer().then(response => {
+        if(response){
+            return fetch(`https://adequate-art-production-2380.up.railway.app/api/${url}`, {
+                method: action,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(body),
+            })
+            .catch(error => {
+                alertShow("Error", "Inconveniente al enviar los datos, se actualizará en breve", "error");
+                setTimeout(() =>{
+                    window.location.reload();
+                }, 3000)
+            });
+        }
+    })
 }
 
 export function receiveData(action, url){
-    return fetch(`https://adequate-art-production-2380.up.railway.app/api/${url}` ,{
-            method: action,
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .catch(error => {
-            alertShow("Error", "Inconveniente al obtener los datos, se actualizará en breve", "error");
-            setTimeout(() =>{
-                window.location.reload();
-            }, 3000)
+    checkServer().then(response => {
+        if(response){
+            return fetch(`https://adequate-art-production-2380.up.railway.app/api/${url}` ,{
+                method: action,
+                headers: { 'Content-Type': 'application/json' },
+            })
+            .catch(error => {
+                alertShow("Error!", "Inconveniente al obtener los datos, se actualizará en breve", "error");
+                setTimeout(() =>{
+                    window.location.reload();
+                }, 3000)
+            });
+        }
+    });
+}
+
+function checkServer() {
+    return fetch("https://adequate-art-production-2380.up.railway.app/api/check")
+        .then(response => response.status === 200)
+        .catch(() => {
+            alertShow("Error!", "Lo sentimos, el servidor no se encuentra disponible", "error")
+            setTimeout(() => {
+                window.location.href = "dashboard.html";
+            }, 3000);
+            return false
         });
 }
